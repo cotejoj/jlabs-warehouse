93257d4954639d9b82b4a6c5e28227ce15320942
cotejoj <jomarcotejo@gmail.com>
2025-10-23 22:07:47 +1100

    update read me

---
+++ ox_inventory.lua
@@ -0,0 +1,60 @@
+['furniture_wardrobe'] = {
+	label = 'Wardrobe',
+	weight = 15000,
+	stack = false,
+	close = true,
+	description = 'A wardrobe for storing clothing.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
+['furniture_shelve'] = {
+	label = 'Storage Shelve',
+	weight = 20000,
+	stack = false,
+	close = true,
+	description = 'A large shelve for storing items.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
+['furniture_table'] = {
+	label = 'Wooden Table',
+	weight = 15000,
+	stack = false,
+	close = true,
+	description = 'A sturdy wooden table.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
+['furniture_chair'] = {
+	label = 'Chair',
+	weight = 8000,
+	stack = false,
+	close = true,
+	description = 'A simple chair.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
+['warehouse_stash'] = {
+	label = 'Storage Box',
+	weight = 10000,
+	stack = false,
+	close = true,
+	description = 'A small storage box for your warehouse.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
+['workbench_item'] = {
+	label = 'Workbench',
+	weight = 25000,
+	stack = false,
+	close = true,
+	description = 'Workbench used for nothing hahaha.',
+	client = {
+		export = 'jlabs-warehouse.placeProp'
+	}
+},
